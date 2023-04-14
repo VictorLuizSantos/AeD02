c100a8acedecb205035020a6ca13eca9ae5ec0f7
Victor Luiz <86701127+VictorLuizSantos@users.noreply.github.com>
2023-04-14 16:12:59 -0400

    Ajuste questao22

--- Lista01/questao22.c
+++ Lista01/questao22.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "questao22.h"
 
-void entrada22(char *cidade, int *eleitores, int *maisVotado) {
-    puts("\n\nDigite o nome da cidade: \n");
-    gets(cidade);
+void entrada22(char **cidade, int *eleitores, int *maisVotado) {
+    printf("\nDigite o o nome da cidade: \n");
+    scanf("%s", cidade);
     printf("\nDigite o total de eleitores: \n");
     scanf("%d", eleitores);
     printf("\nDigite a quantidade de votos do primeiro candidato: \n");
     scanf("%d", maisVotado);
 }
 
-void processamento22(char *cidade, int *eleitores, int *maisVotado) {
+void processamento22(bool *validador, int *eleitores, int *maisVotado) {
+
+    if  (*maisVotado < (*eleitores / 2)) {
+            *validador = true;
+         }
+         else {
+             *validador = false;
+         }
 }
 
-void saida22(char cidade, int eleitores, int maisVotado) {
-    char str[20];
-    cidade = &str[20];
-    if (maisVotado < (eleitores / 2)) {
-    printf("\nHaverá segundo turno em %s\n", cidade);
+void saida22(char cidade, bool validador) {
+
+    if  (validador) {
+    printf("\nHaverÃ¡ segundo turno em %s\n", cidade);
     }
     else {
-         printf("\nNao haverá segundo turno em %s\n", cidade);
+         printf("\nNao haverÃ¡ segundo turno em %s\n", cidade);
     }
 }
 void questao22(void) {
 
-    char str[20];
-    char *cidade = &str[20];
+    char cidade[20];
+    bool validador;
     int eleitores, maisVotado;
 
-    entrada22(cidade, &eleitores, &maisVotado);
-    //processamento22(&cidade, &eleitores, &maisVotado);
-    saida22(cidade, eleitores, maisVotado);
+    entrada22(&cidade, &eleitores, &maisVotado);
+    processamento22(&validador, &eleitores, &maisVotado);
+    saida22(cidade, validador);
 }
