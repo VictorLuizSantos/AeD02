db06c00c7dd6f6212d97eb5455f5ef00f2c59176
VictorLuizSantos <victor_luizsantos@hotmail.com>
2023-05-15 22:14:22 -0400

    Lista02

---
+++ Lista/Lista02/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void entrada() {
+
+}
+
+void processamento() {
+
+}
+
+void saida() {
+
+}
+
+int main()
+{
+    entrada();
+    processamento();
+    saida();
+
+    return 0;
+}
